66f7b51d509394a4fb35cbcfddd5304d095f25f6
tunnckoCore <mameto_100@mail.bg>
2015-02-08 12:11:32 +0200

    dev branch

--- test.js
+++ test.js
@@ -8,3 +8,8 @@
 'use strict';
 
 var gitclone = require('./index');
+
+// gitclone('koajs/koa#0.14.0', 'releases/v0.14.0')
+// gitclone('koajs/koa#0.15.0', 'releases/v0.15.0')
+// gitclone('koajs/koa#0.16.0', 'releases/v0.16.0')
+gitclone('tunnckoCore/koa-better-body#yfghj24', 'koa', true);

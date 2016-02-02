679ccfd6e86234493c20d4088c00166aff8622da
tunnckoCore <mameto_100@mail.bg>
2016-02-02 18:59:32 +0200

    update (pre v2)

--- history.md
+++
@@ -1,2 +0,0 @@
-## v0.0.0 / January 22, 2015
-- init commits
--- test.js
+++ test.js
@@ -1,10 +1,55 @@
-/**
+/*!
  * gitclone <https://github.com/tunnckoCore/gitclone>
  *
- * Copyright (c) 2014-2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) 2014-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
-'use strict';
+/* jshint asi:true */
 
-var gitclone = require('./index');
+'use strict'
+
+var test = require('assertit')
+var gitclone = require('./index')
+
+test('object', function () {
+  gitclone({
+    owner: 'verbose',
+    name: 'verb',
+    branch: 'dev'
+  })
+  // gitclone('jonschlinkert', 'micromatch', true, {dest: 'micro'})
+})
+
+// all of examples below works
+// gitclone({
+//   user: 'foo',
+//   repo: 'bar',
+//   branch: 'zeta'
+// }, true)
+// gitclone({
+//   user: 'foo',
+//   repo: 'bar',
+//   branch: 'dev2'
+// })
+// gitclone({
+//   owner: 'foo',
+//   name: 'bar',
+// }, {dest: 'beta', ssh: true})
+// gitclone({
+//   owner: 'foo',
+//   name: 'bar',
+// }, {dest: 'beta'}, true)
+// gitclone('foo/bar')
+// gitclone('jonschlinkert', 'micromatch', true)
+// gitclone('foo', 'bar', 'dev3')
+// gitclone('foo', 'bar', 'dev3', {dest: 'dest3'})
+// gitclone('foo/bar', {ssh: true})
+// gitclone('foo/bar', {branch: 'opts'})
+// gitclone('foo/bar', {branch: 'opts'}, {ssh: true})
+// gitclone('foo/bar', {branch: 'opts'}, true)
+// gitclone('foo', 'bar', 'baz', true)
+// gitclone('foo/bar', {branch: 'qux'}, true)
+// gitclone('foo/bar#dev', {ssh: true})
+// gitclone('foo/bar#qux', true)
+// gitclone('foo/bar#qux', true, {dest: 'ok'})
